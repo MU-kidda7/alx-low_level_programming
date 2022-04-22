7372c0f0b54ff5c13951d0e3533ff6cbce786dcf
Kidus Mamo <realkoy7@gmail.com>
2022-04-23 01:14:01 +0300

    main

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,43 +1,40 @@
 #include "main.h"
-/* cap_string - a function that capitalizes
- *              all words of a string
+
+/**
+ *cap_string - capitalizes all words of a string.
+ *@s: string to use.
  *
- * @s: pointer to char input array
- *
- * Return: @s
-*/
+ *Return: string.
+ */
 
 char *cap_string(char *s)
 {
-  int i = 0;
+	int i = 1, j, check;
+char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
 
-  /*iterate through our array values*/
-  while (s[i] != '\0')
-  {
-    /*check for any lowercase letters*/
-    if (s[i] >= 97 && s[i] <= 122)
-    {
-      /* if we have a null character
-       * change its value to capital
-      */
-      if (i == 0)
-      {
-        s[i] -= 32;
-      }
-      /**
-       * if we find any character matching the below before any small
-       * letter we change that value to a capital letter.
-      */
-      if (s[i - 1] == 32  s[i - 1] == 9  s[i - 1] == 10 
-        s[i - 1] == 44  s[i - 1] == 59  s[i - 1] == 46 
-        s[i - 1] == 33  s[i - 1] == 63  s[i - 1] == 34 
-        s[i - 1] == 40  s[i - 1] == 41  s[i - 1] == 123 
-        s[i - 1] == 124)
-      {
-        s[i] -= 32;
-      }
-    }
-    i++;
-  }
-  return (s);
+	if (s[0] > 96 && s[0] < 123)
+		s[0] -= 32;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			j = 0;
+			check = 0;
+			while (check == 0 && j < 13)
+			{
+				if (s[i - 1] == a[j])
+				{
+					check = 1;
+				}
+				j++;
+			}
+			if (check == 1)
+			{
+				s[i] -= 32;
+			}
+		}
+		i++;
+	}
+return (s);
 }
