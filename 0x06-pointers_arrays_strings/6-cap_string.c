f1408d08d21c2ab2f5ad5aafe31fa8e12099a89d
Kidus Mamo <realkoy7@gmail.com>
2022-04-23 01:45:21 +0300

    God

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,31 @@
 #include "main.h"
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
+ * cap_string - function that capitalizes all words of a string.
+ * @s: string
+ * Return: output
  */
-
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	if (s[i] >= 97 && s[i] <= 122)
 	{
-		for (i = 0; i < 13; i++)
+		s[i] -= 32;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+		    || s[i] == ',' || s[i] == ';' || s[i] == '.'
+		    || s[i] == '!' || s[i] == '?' || s[i] == '"'
+		    || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		    || s[i] == '}')
 		{
-			if (*(s + count) == sep_words[i])
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
+				s[i + 1] -= 32;
 			}
 		}
-		count++;
+		i++;
 	}
 	return (s);
 }
