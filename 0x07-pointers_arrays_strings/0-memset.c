ffa547777c3e080fb6ca86511843894c0915ada1
Kidus Mamo <realkboy7@gmail.com>
2022-04-25 13:34:08 +0300

    God

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,18 @@
 #include "holberton.h"
+
 /**
- * _memset - fills memory with a constant byte.
- * @s: source string
- * @b: the contant byte for filling
- * @n: lenght of buffer
- * Return: new string.
+ * _memset - it fills a block of memory with constant byte.
+ * @s: a pointer to a memory area.
+ * @b:the constant byte.
+ * @n: the number of bytes of memory to be filled with b.
+ *
+ * Return: s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
-	{
-		*(s + i) = b;
-		i++;
-	}
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
